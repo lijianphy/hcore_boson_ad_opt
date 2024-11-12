f44fc5e35b930094b704d84b83bb164733a32aef
lijianphy <lijianalpha@gmail.com>
2024-11-12 11:35:17 +0800

    Add more configuration files

---
+++ config/print_random_array.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# print an array of random numbers following Gaussian distribution
+
+import random
+import argparse
+import sys
+
+def main():
+    parser = argparse.ArgumentParser(description='Print array of random numbers following Gaussian distribution')
+    parser.add_argument('-a', type=float, help='Mean of the Gaussian distribution', required=True)
+    parser.add_argument('-s', type=float, help='Standard deviation of the Gaussian distribution', required=True)
+    parser.add_argument('-n', type=int, help='Number of random numbers to generate', required=True)
+    
+    args = parser.parse_args()
+    
+    if args.s < 1e-6:
+        print("Error: Standard deviation must be positive")
+        sys.exit(1)
+        
+    random_numbers = [random.gauss(args.a, args.s) for _ in range(args.n)]
+    print(random_numbers)
+
+if __name__ == '__main__':
+    main()

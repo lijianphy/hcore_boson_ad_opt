1e327db38f0c70862c7dac7d6c45b28d8f315fc8
lijianphy <lijianalpha@gmail.com>
2025-01-20 09:21:31 +0800

    Update submit_multiple_job.sh to accept multiple config files and calculate total processes for job submission

--- scripts/submit_mutiple_job.sh
+++ scripts/submit_mutiple_job.sh
@@ -1,31 +1,35 @@
 #!/bin/bash
 
-# Check if config file is provided
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <config_file>"
+export OMP_NUM_THREADS=1
+export LD_LIBRARY_PATH=$HOME/opt/petsc-3.22.1/linux-gnu-complex/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$HOME/opt/slepc-3.22.1/linux-gnu-complex/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$HOME/opt/OpenBLAS/lib:$LD_LIBRARY_PATH
+
+# Check if at least one config file is provided
+if [ "$#" -lt 1 ]; then
+    echo "Usage: $0 <config_file1> [config_file2 ...]"
     exit 1
 fi
 
-CONFIG_FILE=$1
-
-# Define phases array
-phases=(0.0  0.3141592653589793  0.6283185307179586  0.9424777960769379  1.2566370614359172  1.5707963267948966  1.8849555921538759  2.199114857512855  2.5132741228718345  2.827433388230814  3.141592653589793  3.455751918948772  3.7699111843077517  4.084070449666731  4.39822971502571  4.71238898038469  5.026548245743669  5.340707511102648  5.654866776461628  5.969026041820607)
-
-# Loop through all phases
-for phase in "${phases[@]}"; do
-    # Submit job for each phase
-    job_name="phase_${phase}"
-    echo "Submitting job for phase = ${phase}"
+# Loop through all provided config files
+for config_file in "$@"; do
+    # Get base name of config file for job naming
+    base_name=$(basename "${config_file%.*}")
+    job_name="job_${base_name}"
+    # Extract cnt_stream and process_per_stream values from the config file
+    cnt_stream=$(jq '.cnt_stream' $config_file)
+    process_per_stream=$(jq '.process_per_stream' $config_file)
+    # Calculate the total number of processes
+    total_processes=$((cnt_stream * process_per_stream))
+    echo "Submitting job for config = ${config_file} with total processes = ${total_processes}"
     
     sbatch --job-name="${job_name}" \
            --nodes=1 \
-           --ntasks-per-node=2 \
+           --ntasks-per-node="${total_processes}" \
            --partition=batch \
            --output="${job_name}_%j.out" \
-           --wrap="mpirun ./test_optimize ${CONFIG_FILE} ${phase}"
-    
-    # sleep for 2 seconds
-    sleep 2
+           --wrap="mpirun ./optimize_parallel ${config_file}"
 done
 
 echo "All jobs submitted!"
+
